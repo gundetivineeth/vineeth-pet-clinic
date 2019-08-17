dc04f1a19a3735b17174ba9286b06f69224d8da9
gundetivineeth <vineethg508@gmail.com>
2019-08-17 10:43:52 +0530

    Initial project add

---
+++ src/main/java/vineeth/springframework/vineethpetclinic/VineethPetClinicApplication.java
@@ -0,0 +1,13 @@
+package vineeth.springframework.vineethpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VineethPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VineethPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vineeth/springframework/vineethpetclinic/VineethPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package vineeth.springframework.vineethpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VineethPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
