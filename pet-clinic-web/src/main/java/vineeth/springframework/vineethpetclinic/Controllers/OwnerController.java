7179eb72589c4a5dd8c1fde588a7a80805c741d5
gundetivineeth <vineethg508@gmail.com>
2019-08-29 07:34:16 +0530

    List all the Owner and Their Index page of Owners. Closes #12

--- pet-clinic-web/src/main/java/vineeth/springframework/vineethpetclinic/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/vineeth/springframework/vineethpetclinic/Controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package vineeth.springframework.vineethpetclinic.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vineeth.springframework.vineethpetclinic.Models.Owner;
+import vineeth.springframework.vineethpetclinic.services.OwnerService;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
-
-    @RequestMapping({"owners","/","owners/index","owners/index.html"})
-    public String ownerPage()
+    private final OwnerService ownerService;
+    OwnerController(OwnerService ownerService)
     {
+        this.ownerService = ownerService;
+    }
+    @RequestMapping({"","/","index","index.html"})
+    public String ListOwners(Model model)
+    {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
